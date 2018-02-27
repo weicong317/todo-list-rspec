f07b68b0d3ebd4d9a7a7f3ade7bb3daabef5ebd2
audreylky <audrey.lky@gmail.com>
2018-02-27 21:01:09 +0800

    Update task_spec.rb
    Changed it to Given/ When/ Then format

--- task_spec.rb
+++ task_spec.rb
@@ -1,50 +1,49 @@
-
 require "rspec"
-
+require "rspec-given"
 require_relative "task"
 
 describe Task do
 
-  let(:description) { "Walk the giraffe" }
-  let(:task)        { Task.new(description) }
+  Given(:description) { "Walk the giraffe" }
+  Given(:task) { Task.new(description) }
 
   describe "#initialize" do
-    it "takes a description for its first argument" do
-      expect(Task.new("Feed the parakeet")).to be_a_kind_of(Task)
+    context "takes a description for its first argument" do
+      When {Task.new(description)}
+      Then {output.to be_a_kind_of(Task)}
     end
-
-    it "requires one argument" do
-      expect { Task.new }.to raise_error(ArgumentError)
+    
+    context "requires one argument" do
+      When {Task.new}
+      Then {output.to raise_error(ArgumentError)}
     end
   end
-
+  
   describe "#description" do
-    it "returns the correct description for the task" do
-      expect(task.description).to eq description
+    context "returns the correct description for the task" do
+      When {task.description}
+      Then {task.description == description}
     end
   end
-
-  describe "#complete?" do
-    it "returns false for incomplete tasks" do
-      expect(task.complete?).to be(false)
+  
+  descibe "#complete?" do
+    context "returns false for incomplete tasks" do
+      Given {(task).not_to be_complete}
+      When {task.complete?}
+      Then {task.complete? == false}
     end
-
-    it "returns true for completed tasks" do
-      task.complete!
-      expect(task.complete?).to be(true)
+    
+    context "returns true for completed tasks" do
+      Given {(task).to be_complete}
+      When {task.complete!}
+      Then {task.complete? == true}
     end
   end
-
+  
   describe "#complete!" do
-    it "changes the task from incomplete to completed" do
-
-      # This is some RSpec goodness which expects a
-      # method `complete?` to be defined which returns
-      # true or false.
-      #
-      expect(task).not_to be_complete
-      task.complete!
-      expect(task).to be_complete
-    end
+    context "changes the task from incomplete to completed" do
+    When {task.complete!}
+    Then {task.complete? == true}
   end
+    
 end
