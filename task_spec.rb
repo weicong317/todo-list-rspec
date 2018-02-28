d88fbe27297df1b5194f0fe5d8d807a75766852c
audreylky <audrey.lky@gmail.com>
2018-02-28 14:31:23 +0800

    Update task_spec.rb

--- task_spec.rb
+++ task_spec.rb
@@ -9,41 +9,37 @@ describe Task do
 
   describe "#initialize" do
     context "takes a description for its first argument" do
-      When {Task.new(description)}
-      Then {Task.new(description)).to be_a_kind_of(Task)}
+      When (:output){Task.new(description)}
+      Then {expect(output).to be_a_kind_of(Task)}
     end
     
     context "requires one argument" do
-      When {Task.new}
-      Then {expect{ Task.new }.to raise_error(ArgumentError)}  # noticed we used {Task.new} instead of (Task.new)? With () is to test the return value, {} is to test the behavior
+      Then {expect{Task.new}.to raise_error(ArgumentError)} # noticed we used {Task.new} instead of (Task.new)? With () is to test the return value, {} is to test the behavior
     end
   end
   
   describe "#description" do
     context "returns the correct description for the task" do
-      When {task.description}
-      Then {task.description == description}  # Rspec matcher version: expect(task.description).to eq description
+      Then {task.description == description}
     end
   end
   
-  descibe "#complete?" do
+  describe "#complete?" do
     context "returns false for incomplete tasks" do
-      Given {(task).not_to be_complete}
-      When {task.complete?}
       Then {task.complete? == false}  # Rspec matcher version: expect(task.complete?).to be(false)
     end
     
     context "returns true for completed tasks" do
-      Given {(task).to be_complete}
-      When {task.complete?}
+      When {task.complete!}
       Then {task.complete? == true}
     end
   end
   
   describe "#complete!" do
     context "changes the task from incomplete to completed" do
-    When {task.complete!}
-    Then {task.complete? == true}
+      Then {task.complete? == true}
+      When {task.complete!}
+      Then {task.complete? == true}
+    end
   end
-    
 end
